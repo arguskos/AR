d2fb3a6538bc58675d5830e44415149988f2d40a
WouterWeynants <wouter.weynants@hotmail.com>
2017-06-14 14:15:30 +0200

    UI Proof Of Concept

---
+++ Assets/Scripts/Visuals/UIProofOfConcept.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIProofOfConcept : MonoBehaviour {
+
+    public GameObject Canvas;
+    private float Timer;
+    public float MaxTime;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer>= MaxTime)
+        {
+            Canvas.GetComponent<Animator>().enabled = false;
+        }
+
+    }
+}
